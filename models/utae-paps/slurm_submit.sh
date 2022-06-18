7a84755b0839b42a950cb31a2d7552856db37256
Patrick <patrick.ebel@tum.de>
2022-06-18 16:19:28 +0200

    changed slurm script

--- models/utae-paps/slurm_submit.sh
+++ models/utae-paps/slurm_submit.sh
@@ -4,4 +4,10 @@
 #SBATCH --time=6-23:59:59
 #SBATCH --output=job.out
 
-srun python train_reconstruct.py --root1 /net/cephfs/home/pebel/scratch/SEN12MSCRTS --root2 /net/cephfs/home/pebel/scratch/SEN12MSCRTS_val_test --input_t 5 --region all --loss combined --perceptual None --experiment_name utae_S1S2_t5_L1SSIM_all
+srun python train_reconstruct.py    --root1 /net/cephfs/home/pebel/scratch/SEN12MSCRTS 
+                                    --root2 /net/cephfs/home/pebel/scratch/SEN12MSCRTS_val_test 
+                                    --input_t 4     
+                                    --region all 
+                                    --loss combined
+                                    --perceptual None
+                                    --experiment_name utae_S1S2_t4_L1SSIM_all
