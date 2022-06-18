b14e68c753eb2a17dce2e7a2e2f0bc1c4964b08e
Patrick <patrick.ebel@tum.de>
2022-06-18 17:25:26 +0200

    changed slurm config

--- models/utae-paps/slurm_submit.sh
+++ models/utae-paps/slurm_submit.sh
@@ -3,6 +3,6 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=15000
 #SBATCH --time=6-23:59:59
-#SBATCH --output=job.out
+#SBATCH --output=job2.out
 
 srun python train_reconstruct.py --root1 /net/cephfs/home/pebel/scratch/SEN12MSCRTS --root2 /net/cephfs/home/pebel/scratch/SEN12MSCRTS_val_test --input_t 3 --region all --loss combined --perceptual None --experiment_name utae_S1S2_t3_L1SSIM_all
